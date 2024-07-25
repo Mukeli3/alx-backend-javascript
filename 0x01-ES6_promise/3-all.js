09c5923bdb26bf1cd3f3f567d3e0251c0dd1b75c
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-25 02:26:10 +0000

    all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
