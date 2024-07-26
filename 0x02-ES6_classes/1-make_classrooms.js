2ad6ab9cfbe23b2683824d9ddd4647d4dbe7a2ca
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-26 22:21:31 +0000

    fixed lint, camel case

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const f_rm = new ClassRoom(19);
-  const s_rm = new ClassRoom(20);
-  const t_rm = new ClassRoom(34);
+  const fRm = new ClassRoom(19);
+  const sRm = new ClassRoom(20);
+  const tRm = new ClassRoom(34);
 
-  return [f_rm, s_rm, t_rm];
+  return [fRm, sRm, tRm];
 }
