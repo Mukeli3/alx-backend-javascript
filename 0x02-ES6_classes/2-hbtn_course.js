abf9477a8a205fa53a58448e9860b6c44857f5be
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-26 02:15:19 +0000

    refactor array of Strings

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,9 +6,6 @@ export default class HolbertonCourse {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
