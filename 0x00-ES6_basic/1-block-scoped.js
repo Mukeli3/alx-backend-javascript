c561f961feaa3c40d22335159ae3de3a3b121dfe
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-27 00:34:34 +0000

    lint fix attempt

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,6 +5,8 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    console.log(task);
+    console.log(task2);
   }
 
   return [task, task2];
