2b8a7b9b59fdd98793f873ed9be930934483f7d6
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-30 23:52:23 +0000

    fixed eslint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (let [item, quant] of map) {
+  for (const [item, quant] of map) {
     if (quant === 1) {
       map.set(item, 100);
     }
