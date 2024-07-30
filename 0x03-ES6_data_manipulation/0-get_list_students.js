02e266fbba29e253cd9b9eb9f936487a79a44dc4
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-30 19:26:05 +0000

    used double "s

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
   const studes = [
-    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 5, firstname: 'Serena', location: 'San Francisco' }
+    { id: 1, firstname: "Guillaume", location: "San Francisco" },
+    { id: 2, firstname: "James", location: "Columbia" },
+    { id: 5, firstname: "Serena", location: "San Francisco" },
   ];
   return studes;
 }
