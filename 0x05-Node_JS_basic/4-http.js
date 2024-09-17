a95f17ced14ab550d4ead9ae808872530820ff6e
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-17 23:33:23 +0000

    killed one

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 
-const app = http.createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
