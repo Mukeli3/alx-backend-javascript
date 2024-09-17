1d12edba20649221b682ea5faf51e66c3f87394e
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-17 23:08:30 +0000

    not readline

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,10 @@
-const readline = require('node:readline');
+console.log('Welcome to Holberton School, what is your name?');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  rl.close();
+  process.exit();
 });
-rl.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
