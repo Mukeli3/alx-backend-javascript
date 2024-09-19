05288e1baa04684c444bc1f86652f5c6a066752e
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-19 07:11:42 +0000

    console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\r`);
+  console.log('');
   process.exit();
 });
 process.on('exit', () => {
