b282ab286f052214e6ec9b45838f03f0c8a5743d
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-19 22:40:47 +0000

    one int one float

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,6 +8,9 @@ describe('calculateNumber', function() {
   it('should return 6 when inputs are 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('should return 5 when inputs are 1 and 4.3', function() {
+    assert.strictEqual(calculateNumber(1, 4.3), 5);
+  });
   it('should return 4 when inputs are 1 and 3', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
