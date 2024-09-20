a193dda85840ed65f8d0705e7d46793092016869
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-20 21:20:43 +0000

    asymchronous

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lns = data.trim().split('\n');
+      const students = lns.slice(1).filter(line => line);
+      
+      console.log(`Number of students: ${students.length}`);
+
+      const fds = {};
+
+      students.forEach((stud) => {
+        const [fname, , , fd] = stud.split(',');
+
+        if (!fds[fd]) {
+          fds[fd] = [];
+	}
+	fds[fd].push(fname);
+      });
+      Object.keys(fds).forEach((fd) => {
+        console.log(`Number of students in ${fd}: ${fds[fd].length}. List: ${fds[fd].join(', ')}`);
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
