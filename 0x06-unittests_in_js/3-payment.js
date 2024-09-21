f1bc8bef7854840ee5a8c04dfd98b5442579d5cc
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-21 02:59:22 +0000

    test

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,3 +1,6 @@
+const Utils = require('./utils');
+
+
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${res}`);
