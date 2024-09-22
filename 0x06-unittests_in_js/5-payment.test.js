396c4e5d916e3aa730138b504942b04486166419
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-22 23:33:10 +0000

    fixed malformed function and spelling check

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -15,12 +15,12 @@ describe('sendPaymentRequestToApi', () => {
     consSpy.restore();
   });
 
-  it('logs "The total is: 120" and calls console once with 100 and 20 as inputs') => {
+  it('logs "The total is: 120" call console once with 100, 20 as inputs', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(consoSpy.calledOnce).to.be.true;
+    expect(consSpy.calledOnce).to.be.true;
     expect(consSpy.calledWith('The total is: 120')).to.be.true;
   });
-  it('logs "The total is: 20" call console once with 10 and 10 as inputs', () => {
+  it('logs "The total is: 20" call console once with 10, 10 as inputs', () => {
     sendPaymentRequestToApi(10, 10);
 
     expect(consSpy.calledOnce).to.be.true;
